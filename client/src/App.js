1e646dc41c2b744911945761a29237fa514102dd
keren0908 <akayayy@gmail.com>
2018-09-28 11:27:46 -0400

    add auth part for client

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
 class App extends Component {
@@ -14,6 +17,10 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </div>
           <Footer />
         </div>
       </Router>
