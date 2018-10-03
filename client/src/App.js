6a67292bac6a8b6043463c4699d213829b715584
keren0908 <akayayy@gmail.com>
2018-10-03 15:52:41 -0400

    add education form & functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser,logoutUser } from './actions/authActions';
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import { Provider } from "react-redux";
-import store from './store';
-import PrivateRoute from './components/common/PrivateRoute';
+import store from "./store";
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -14,17 +14,16 @@ import Footer from "./components/layout/Footer";
 
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/create-profile/CreateProfile';
-import EditProfile from './components/edit-profile/EditProfile';
-import AddExperience from './components/add-credentials/AddExperience';
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
 
-
-
 // Check for token
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
   // Set auth Token header auth
   setAuthToken(localStorage.jwtToken);
   // Decode the token and get user info and exp
@@ -34,7 +33,7 @@ if(localStorage.jwtToken){
 
   //check for expired token
   const currentTime = Date.now() / 1000;
-  if(decoded.exp < currentTime) {
+  if (decoded.exp < currentTime) {
     //logout user
     store.dispatch(logoutUser());
     //clear current profile
@@ -42,14 +41,14 @@ if(localStorage.jwtToken){
 
     // TODO: clear current profile
     // Redirect to login
-    window.location.href ='login';
+    window.location.href = "login";
   }
 }
 
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
+      <Provider store={store}>
         <Router>
           <div className="App">
             <Navbar />
@@ -61,13 +60,32 @@ class App extends Component {
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
               <Switch>
-                <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path='/add-experience' component={AddExperience} />
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
               </Switch>
             </div>
             <Footer />
